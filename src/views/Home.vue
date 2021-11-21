e5976df2016bc6bcc2bfbf846393f0b52dd19aee
budi <budi.kerja8@gmail.com>
2021-11-21 14:06:30 +0700

    upload first

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+      <HeaderShayna />
+      <HeroShayna />
+      <ProductShayna />
+      <InstaShayna />
+      <PartnerShayna />
+      <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import ProductShayna from '@/components/ProductShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnerShayna from '@/components/PartnerShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    ProductShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
